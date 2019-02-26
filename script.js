c8ad553227b4c4ae787c9d58ef4190ee3a19a862
kevincastle <kevinacastles@gmail.com>
2019-02-26 11:35:56 -0300

    V 1.0

---
+++ script.js
@@ -0,0 +1,10 @@
+$(".player").on('touchmove', function (e) {
+    var x = e.touches[0].pageX;
+    $("#spaceship").css('left', x);
+});
+
+$(document).on('mousemove touchmove', (event) => {
+    $('#spaceship').css({
+        left: event.clientX
+    });
+});
